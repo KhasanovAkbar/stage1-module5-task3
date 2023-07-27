9e2881dd2318643e9c09dc623576615aa0309ba9
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-07-27 14:22:17 +0500

    first commit clean up project

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,11 +1,9 @@
 package com.epam.mjc;
 
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CollectionService {
 
